aed4830414ca268adf0b90d088ce5e0b15e581e5
Gaston Tomas Huete <44206933+GastonTH@users.noreply.github.com>
2021-09-17 18:21:00 +0200

    Update ej6.py

--- Practica 1/ej6.py
+++ Practica 1/ej6.py
@@ -5,19 +5,31 @@ Escribir un pequeño programa donde:
 - Se calcula cuántos años cumplirán durante el año en curso.
 - Se imprime en pantalla"""
 
-import datetime
+from datetime import datetime, timedelta
 
-#anyo_curso = input("Dime el anyo del curso")
+from dateutil.relativedelta import relativedelta
 
-hoy = datetime.datetime.today()
-print("hoy", hoy)
+anyo_curso = 2021
+
+hoy = datetime.now()
+fin_curso = datetime(anyo_curso, 6, 22)
+#print("Hoy:", hoy, "\nFinal de curso", fin_curso)
 
 lista_persona_anyo = {
-    'Alumno1' : 'Pepe', 'Fecha1' : datetime.datetime(1999,2,6),
-    'Alumno2' : 'Carlos', 'Fecha2' : datetime.datetime(1999,11,1),
-    'Alumno3' : 'Juan', 'Fecha3' : datetime.datetime(1993,7,29),
+    'Alumno1' : 'Pepe', 'Fecha1' : datetime(1999,2,6),
+    'Alumno2' : 'Carlos', 'Fecha2' : datetime(1999,11,1),
+    'Alumno3' : 'Juan', 'Fecha3' : datetime(1993,7,29),
 }
+
+"""print(hoy)
 
 for persona, fecha in lista_persona_anyo.items():
 
     print(persona, "-", fecha)
+"""
+
+ahora = datetime.now()
+hace_una_semana = ahora - timedelta(year=7)
+print(hace_una_semana)
+
+"""relativedelta <---<----<<-<<-<<--<<<-<<--<<-----------------------------------"""
